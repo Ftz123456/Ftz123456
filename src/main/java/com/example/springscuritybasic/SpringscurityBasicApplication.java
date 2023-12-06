74f2ef7e4852c13000ae4409069c032f86b8b7f8
fantianzhi <485913144@qq.com>
2023-12-06 10:12:58 +0800

    创建

---
+++ src/main/java/com/example/springscuritybasic/SpringscurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringscurityBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springscuritybasic/SpringscurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springscuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringscurityBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
